1d746eca1ef6c5c6931f3b6d48507f50ba9e32f6
sidaswal7 <siddhantaswal7@gmail.com>
2024-05-25 19:40:54 +0530

    fixed expenses list

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const ExpenseItem = ({money,description,category})=>{
     return(
-        <li className="flex justify-evenly">
-            <h2>Money Spent: ${money}</h2>
-            <h2>Description: {description}</h2>
-            <h2>Category: {category}</h2>
+        <li className="flex pl-32 mb-2">
+            <h2 className="text-md font-medium basis-1/3">Money Spent: ${money}</h2>
+            <h2 className="text-md font-medium basis-1/3">Description: {description}</h2>
+            <h2 className="text-md font-medium basis-1/3">Category: {category}</h2>
         </li>
     )
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,15 +5,15 @@ import ExpenseItem from "./ExpenseItem";
 const dummy_data = [{
     money:2000,
     description:"chicken biryani",
-    category:"food"
+    category:"Food"
 },{
     money:3000,
     description:"lagaan",
-    category:"movie"
+    category:"Movie"
 },{
     money:4000,
     description:"trip to mumbai",
-    category:"travel"
+    category:"Travel"
 }]
 const Expenses = ()=>{
     const [showForm, setShowForm] = useState(false);
@@ -24,11 +24,14 @@ const Expenses = ()=>{
         setShowForm(false);
     }
     return(
-        <section className="flex justify-center items-center">
+        <section>
             <div className="flex justify-center items-center flex-col">
                 {!showForm && <button className="px-2 py-1 bg-green-500 hover:bg-green-600 text-white rounded shadow-md" onClick={()=>setShowForm(true)}>Show Form</button>}  
                 {showForm && <ExpenseForm onHideForm={()=>setShowForm(false)} onAddExpense = {addExpenseHandler}/>} 
-                <ul>
+            </div>
+            <div>
+                <ul className="border-2 border-slate-600 p-4 mt-10 mx-8 rounded-md shadow">
+                    <h1 className="text-xl font-medium text-center mb-5">Expense List</h1>
                     {expenses.map((item)=><ExpenseItem
                         money={item.money}
                         description={item.description}
